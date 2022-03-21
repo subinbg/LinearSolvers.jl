2cc6193442343c42a6d639993819d86e68c96068
Subeen Pang <subinbg@users.noreply.github.com>
2022-03-20 21:41:32 -0400

    Update LinearSolvers.jl

--- src/LinearSolvers.jl
+++ src/LinearSolvers.jl
@@ -15,17 +15,21 @@ using Printf
 
 import Base: iterate
 
-mul!(x::Vector, A::Matrix, y::Vector) = begin x .= A * y end
-mul!(x::Vector, y::Number, z::Vector) = begin x .= y .* z end
-sub!(x::Vector, y::Vector, z::Vector) = begin x .= y .- z end
-add!(x::Vector, y::Vector, z::Vector) = begin x .= y .+ z end
+abstract type VectorLike end
+abstract type MatrixLike end
+abstract type ScalarLike end
+
+mul!(x::VectorLike, A::MatrixLike, y::VectorLike) = begin x .= A * y end
+mul!(x::VectorLike, y::ScalarLike, z::VectorLike) = begin x .= y .* z end
+sub!(x::VectorLike, y::VectorLike, z::VectorLike) = begin x .= y .- z end
+add!(x::VectorLike, y::VectorLike, z::VectorLike) = begin x .= y .+ z end
 dot(x::Nothing, y::Nothing) = nothing # LinearAlgebra.dot(x,y)
 norm(x::Nothing) = nothing # LinearAlgebra.norm(x)
-add_scmul!(x::Vector, scalar::Number, y::Vector) = begin x .+= scalar .* y end
-sub_scmul!(x::Vector, scalar::Number, y::Vector) = begin x .-= scalar .* y end
+add_scmul!(x::VectorLike, scalar::ScalarLike, y::VectorLike) = begin x .+= scalar .* y end
+sub_scmul!(x::VectorLike, scalar::ScalarLike, y::VectorLike) = begin x .-= scalar .* y end
 eltype(x::Nothing) = nothing # Base.eltype(x)
 size(x::Nothing) = nothing # Base.size(x)
-fill!(x::Nothing, scalar::Number) = nothing # Base.fill!(x, scalar)
+fill!(x::Nothing, scalar::ScalarLike) = nothing # Base.fill!(x, scalar)
 rand!(x::Nothing) = nothing # Random.rand!(x)
 copyto!(x::Nothing, y::Nothing) = nothing # Base.copyto!(x, y)
 
